a8a157caafa0c6fd8059c831c9e6ddb29d5ee92a
0101100 <128458926+swerce@users.noreply.github.com>
2023-09-06 15:49:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,29 @@
 from flask import Flask, request, jsonify
-from PIL import Image
-import os
+from rembg import remove
+from flask_cors import CORS
+
+
+
+
+model_path = "/u2net.onnx"
 
 app = Flask(__name__)
+CORS(app)
 
-def convert_image(input_path, output_path, output_format):
+@app.route('/remove_background', methods=['POST'])
+def remove_background():
     try:
-        image = Image.open(input_path)
-        image.save(output_path, format=output_format)
-        return True, None
+        # Get the image from the request
+        image = request.files['image']
+        
+        # Process the image using rembg
+        image = remove(image.read())
+        
+        # Return the processed image
+        response = app.response_class(response=image, content_type='image/png')
+        return response
     except Exception as e:
-        return False, str(e)
-
-@app.route('/convert', methods=['POST'])
-def image_converter():
-    input_image_format = request.form.get('input_format', 'JPEG')
-    output_image_format = request.form.get('output_format', 'PNG')
-
-    input_image_file = request.files['image']
-    input_image_path = 'input_image_temp'
-    input_image_file.save(input_image_path)
-
-    output_image_path = 'output_image_temp.' + output_image_format.lower()
-    success, error_message = convert_image(input_image_path, output_image_path, output_image_format)
-
-    if success:
-        return jsonify({"message": "Conversion successful.", "output_image_path": output_image_path}), 200
-    else:
-        return jsonify({"error": "Conversion failed.", "message": error_message}), 500
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
